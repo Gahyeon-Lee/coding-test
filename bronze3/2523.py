0a4ed9804629809a96664f3bad8d5f4206e217aa
Gahyun Lee <ab3380@naver.com>
2022-09-27 21:28:36 +0900

    9/27

---
+++ bronze3/2523.py
@@ -0,0 +1,11 @@
+# 별 찍기 - 13
+# 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+# 첫째 줄부터 2×N-1번째 줄까지 차례대로 별을 출력한다.
+
+num = int(input())
+
+for i in range(1, num + 1):
+    print('*' * i)
+
+for i in range(num - 1, 0, -1):
+    print('*' * i)
