a48fc418133fef0af545fa8c825b2e8ed10d8431
Gahyun Lee <gahyun608@gmail.com>
2023-01-30 13:35:55 +0900

    06 sorting 성적이 낮은 순서로 학생 출력하기

--- 06 sorting/6-11.py
+++ 06 sorting/6-11.py
@@ -1,3 +1,4 @@
+# 6-11.py
 # 성적이 낮은 순서로 학생 출력하기
 
 n = int(input())
@@ -11,3 +12,8 @@ score = sorted(score, key=lambda student:student[1])
 
 for i in score:
     print(i[0], end=' ')
+    
+"""
+def 함수이름(매개변수): lamda 매개변수: 결과
+return 결과 
+"""
