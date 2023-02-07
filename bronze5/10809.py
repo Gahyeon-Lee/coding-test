04110266657bf6b9467b34bf016fd8457a1a2bbd
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-02-07 22:02:35 +0900

    알파벳 찾기

---
+++ bronze5/10809.py
@@ -0,0 +1,8 @@
+# 10809.py
+# 알파벳 찾기
+
+s = input()
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' , 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+for i in alphabet:
+    print(s.find(i), end=' ')
