603d812ed71e8d64f36572645e8d297a78417494
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-02-07 22:01:51 +0900

    OX 퀴즈

---
+++ bronze2/8958.py
@@ -0,0 +1,16 @@
+# 8958.py
+# OX 퀴즈
+
+n = int(input())
+
+for _ in range(n):
+    ox_list = list(input())
+    score = 0
+    sum_score = 0
+    for i in ox_list:
+        if i == 'O':
+            score += 1
+            sum_score += score
+        else:
+            score = 0
+    print(sum_score)
