dd9fde5f1e523d73da1b6a68ea78d4331c811014
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-02-23 21:57:46 +0900

    부녀회장이 될테야(수정)

--- 백준/bronze1/2775.py
+++ 백준/bronze1/2775.py
@@ -7,12 +7,10 @@ for i in range(t):
     k = int(input())
     n = int(input())
     
-    d = [0] * (n + 1)
-    for j in range(1, n + 1):
-        d[j] = j
+    d = [i for i in range(1, n + 1)]
         
     for m in range(k):
-        for n in range(1, n + 1):
-            d[n] += d[n - 1]
+        for b in range(1, n):
+            d[b] += d[b - 1]
             
-print(d[-1])
+    print(d[-1])
