b89e5d1290c0022ee70b522cc35a527783e60061
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-09 11:25:13 +0900

    DFS와 BFS

--- 백준/silver2/1260.py
+++ 백준/silver2/1260.py
@@ -1,45 +1,39 @@
-import queue
+# 1260.py
+# DFS와 BFS
 
-N, M, V = map(int, input().split())
+from collections import deque
+import sys
+read = sys.stdin.readline
 
-# 인접 영행렬 생성
-matrix = [[0] *  (N + 1) for i in range(N + 1)]
+def bfs(v):
+  q = deque()
+  q.append(v)       
+  visit_list[v] = 1   
+  while q:
+    v = q.popleft()
+    print(v, end = " ")
+    for i in range(1, n + 1):
+      if visit_list[i] == 0 and graph[v][i] == 1:
+        q.append(i)
+        visit_list[i] = 1
 
-# 방문한 곳을 확인하기 위한 배열 선언
-visited = [0] * (N + 1)
+def dfs(v):
+  visit_list2[v] = 1        
+  print(v, end = " ")
+  for i in range(1, n + 1):
+    if visit_list2[i] == 0 and graph[v][i] == 1:
+      dfs(i)
 
-# 입력 받는 두 값에 대해 영행렬에 1 삽입
-for i in range(M):
-    num1, num2 = map(int, input().split())
-    matrix[num1][num2] = matrix[num1][num2] = 1
-    
-def dfs(V):
-    # 방문한 곳은 1 넣기
-    visited[V] = 1
-    print(V, end=' ')
-    
-    # 재귀함수 선언(V와 인접한 곳을 찾고 방문하지 않았다면 실행)
-    for i in range(1, N+1):
-        if (visited[i] == 0 and matrix[V][i] == 1):
-            dfs(i)
-           
-            
-def bfs(V):
-    # 방문해야 할 곳은 순서대로 넣을 큐
-    queue = [V]
-    
-    # dfs를 완료한 visited 배열(1로 되어있음)에서 0으로 방문 체크 -> 초기화해줘야 함
-    visited[V] = 0
-    
-    # 큐 안에 데이터가 없어질 때까지
-    while queue:
-        V = queue.pop(0);
-        print(V, end=' ')
-        for i in range(1, N+1):
-            if (visited[i] == 1 and matrix[V][i] == 1):
-                queue.append(i)
-                visited[i] = 0
+n, m, v = map(int, read().split())
 
-dfs(V)
+graph = [[0] * (n + 1) for _ in range(n + 1)] 
+visit_list = [0] * (n + 1)
+visit_list2 = [0] * (n + 1)
+
+for _ in range(m):
+  a, b = map(int, read().split())
+  graph[a][b] = graph[b][a] = 1
+
+dfs(v)
 print()
-bfs(V)
+bfs(v)
