5d4a8ff3182e6b86ee55e9fe1f8ea9fe6171cf91
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-06-02 14:20:06 +0900

    세탁소 사장 동혁

---
+++ 백준/브론즈/bronze3/2720.py
@@ -0,0 +1,14 @@
+# 2720.py
+# 세탁소 사장 동혁
+
+coins = [25, 10, 5, 1]
+
+t = int(input())
+
+for _ in range(t):
+    c = int(input())
+    
+    for coin in coins:
+        print(c // coin, end=' ')
+        c %= coin
+    print("")
