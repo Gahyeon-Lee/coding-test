df94b854dfb49a6be0ab75c863b6a897edfcb44f
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-06-03 14:33:37 +0900

    블랙잭

---
+++ 백준/브론즈/bronze2/2798.py
@@ -0,0 +1,16 @@
+# 2798.py
+# 블랙잭
+
+n, m = map(int, input().split())
+cards = list(map(int, input().split()))
+
+result = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            total = cards[i] + cards[j] + cards[k]
+            
+            if total <= m:
+                result = max(result, total)
+
+print(result)
