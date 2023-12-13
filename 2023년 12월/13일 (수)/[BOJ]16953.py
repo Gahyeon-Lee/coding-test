25ab6a97b58dd00bf6ab3c37112bf8c0cbc267f4
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-12-13 12:23:35 +0900

    A → B / 실버2

---
+++ 2023년 12월/13일 (수)/[BOJ]16953.py
@@ -0,0 +1,21 @@
+# 16953.py
+# A → B
+
+a, b = map(int, input().split())
+
+count = 1
+while True:
+    if a == b:
+        break
+    elif (b % 2 != 0 and b % 10 != 1) or (b < a):
+        count = -1
+        break
+    else:
+        if b % 2 == 0:
+            b //= 2
+            count += 1
+        elif b % 10 == 1:
+            b //= 10
+            count += 1
+
+print(count)
