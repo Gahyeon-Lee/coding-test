4ae7b01af9f3201a5e1046d58fc22c5b1dcfc11e
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-12-19 10:18:10 +0900

    모험가 길드 두 번째 풀어보기

---
+++ 이것이 코딩테스트다/03 greedy/모험가 길드 2.py
@@ -0,0 +1,16 @@
+# 모험가 길드 두 번째 풀어보기
+
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+result = 0
+count = 0
+
+for i in data:
+    count += 1
+    if count >= i:
+        result += 1
+        count = 0
+
+print(result)
