646117e3c0c5250a1511c51ca745d162f0003dcd
Gahyeon-Lee <gahyeon.plum@gmail.com>
2024-01-16 14:13:21 +0900

    실버3 / N과 M (5)

---
+++ 2024년 1월/16일 (화)/[BOJ]15654.py
@@ -0,0 +1,19 @@
+# 15654.py
+# N과 M (5)
+
+n, m = map(int, input().split())
+numbers = sorted(list(map(int, input().split())))
+s = []
+
+def dfs():
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    
+    for i in numbers:
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
+            
+dfs()
