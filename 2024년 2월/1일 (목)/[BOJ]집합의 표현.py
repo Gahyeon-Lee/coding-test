11251d945114f2935da7cfceb5e0f142c826a981
Gahyeon-Lee <gahyeon.plum@gmail.com>
2024-02-02 13:19:03 +0900

    골드5 / 집합의 표현

---
+++ 2024년 2월/1일 (목)/[BOJ]집합의 표현.py
@@ -0,0 +1,34 @@
+# 1717.py
+# 집합의 표현
+
+import sys
+sys.setrecursionlimit(1000000)
+input = sys.stdin.readline
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+        
+n, m = map(int, input().split())
+parent = [i for i in range(n + 1)]
+
+for _ in range(m):
+    num, a, b = map(int, input().split())
+    
+    if num == 0:
+        union(a, b)
+    else:
+        if find(a) == find(b):
+            print('YES')
+        else:
+            print('NO')
