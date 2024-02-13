d676ab5d2ec93bb1e585995a4cbeb6d979e7dae5
Gahyeon-Lee <gahyeon.plum@gmail.com>
2024-02-13 16:04:49 +0900

    화성 탐사

---
+++ 2024년 2월/9일 (금)/[이코테]화성 탐사.py
@@ -0,0 +1,40 @@
+# 화성 탐사
+
+INF = int(1e9)
+
+import sys, heapq
+input = sys.stdin.readline
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    graph = [list(map(int, input().split())) for _ in range(n)]
+    distance = [[INF] * n for _ in range(n)]   
+    
+    x, y = 0, 0
+    q = [(graph[x][y], x, y)]
+    distance[x][y] = graph[x][y]
+    
+    while q:
+        dist, x, y = heapq.heappop(q)
+        
+        if distance[x][y] < dist:
+            continue
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 <= nx < n and 0  <= ny < n:
+                cost = dist + graph[nx][ny]
+                
+                if cost < distance[nx][ny]:
+                    distance[nx][ny] = cost
+                    heapq.heappush(q, (cost, nx, ny))
+    
+    print(distance[n - 1][n - 1])
+        
